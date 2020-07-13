a5ead91009208c88b44519ef699e58527ff89ba9
501994486@qq.com <501994486@qq.com>
2020-07-13 20:40:21 +0800

    订单新增

--- order-service/src/main/java/com/tmall/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/tmall/order/OrderServiceApplication.java
@@ -1,15 +1,19 @@
 package com.tmall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableTransactionManagement
+@MapperScan("com.tmall.order.infrastructure.dao")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
