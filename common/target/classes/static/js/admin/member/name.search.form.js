0c0a460dce984f6a963d6909cc7555f6e0226918
501994486 <501994486@qq.com>
2020-07-29 16:20:36 +0800

    删除了target

--- common/target/classes/static/js/admin/member/name.search.form.js
+++
@@ -1,7 +0,0 @@
-	$("ul.menu li").hover(function(){
-	$(">ul:not(:animated)",this).slideDown("fast");
-	},
-	function(){
-		$(">ul",this).slideUp("fast");
-	});
-	$("ul.sub").parent("li").children("a").addClass("arrow-sub");
--- common/target/classes/static/js/common/ckeditor/plugins/ysLineHeight/lang/ja.js
+++
@@ -1,5 +0,0 @@
-﻿CKEDITOR.plugins.setLang('ysLineHeight', 'ja', {
-    label: '行間',
-    voiceLabel: '行間',
-    panelTitle: '行間'
-});
